cdc85187bf2ced2aa3c4176d8c4c1f3d6fac2b49
ftherrien <felix.therrien@gmail.com>
2018-08-09 11:33:10 -0600

    Minor corrections to __init__.py

--- pythonQE/__init__.py
+++ pythonQE/__init__.py
@@ -1,2 +1,5 @@
-from jobs import pwcalc, phcalc, q2rcalc, matcalc, dyncalc, submit_jobs
-from prepare import reciprocal, on_path, to_relaxed, derivative_points, all_points
+from jobs import (pwcalc, phcalc, q2rcalc,
+                  matcalc, dyncalc, submit_jobs)
+from prepare import (reciprocal, on_path, to_relaxed_coord,
+                     derivative_points, all_points, explicit_path,
+                     unique)
